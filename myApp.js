7c4e197b950063f8dacadd8cffd0bf319190fc29
101jah <101jah@proton.me>
2024-05-28 14:25:57 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
-
-
+const PORT = process.env.PORT || 3030;
 
 
 
@@ -57,5 +57,6 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`101jah Info Security server started on port ${PORT}`);
 });
+git 
