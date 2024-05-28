d144ef9936a55261fa6f3be245f6e6cb01f1b269
101jah <101jah@proton.me>
2024-05-28 14:54:14 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -53,7 +53,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3001;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`101jah Info Security server started on port ${port}`);
 });
